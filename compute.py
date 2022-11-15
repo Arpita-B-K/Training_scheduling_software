f6ff859af5e408bf3960d74ca09f4144188f27ea
hitheshdnayak <hitheshdnayak@gmail.com>
2022-11-15 13:16:37 +0530

    commit 3

--- compute.py
+++ compute.py
@@ -1,7 +1,29 @@
 
 
+def sort_fun(sk):
+    l=[]
+    for ele in sk:
+        if(ele != ','){
+            l.append(int(ele))
+        }
+    l = l.sort()
+    return l
+        
+        
+
 def create_sch():
-    pass
+    print("enter the skill set number seperated by commas")
+    skills=""
+    print("1)python  2)C  3)C++   4)ruby")
+    print("5)algorithms    6)machine-learning    7)datascience  8)Bigdata")
+    print("9)leadership-qualities   10)supportive-member  ")
+    s = input("enter number (to stop enter \'q\'): ")
+    skills = skills+s+","
+    while(s != 'q'):
+            s = input("enter number (to stop enter \'q\'): ")
+            skills = skills+s+","
+    sort_fun(skills)            
+    get_training(skills)
 
 def print_sch():
     pass
