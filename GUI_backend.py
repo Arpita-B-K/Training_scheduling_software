2b811e124df51294a007b3857b112e9740c42143
hitheshdnayak <hitheshdnayak@gmail.com>
2022-11-26 12:38:27 +0530

    commit17

--- GUI_backend.py
+++ GUI_backend.py
@@ -1,10 +1,11 @@
 from GUI import *
 
-def input_through_gui():
-	app = window()
+def input_through_gui(user_id):
+	app = signup_page()
 	app.geometry("500x500")
 	app.mainloop()
+    
 
 
 if __name__ == "__main__":
-    input_through_gui()
+    input_through_gui("ID1")
